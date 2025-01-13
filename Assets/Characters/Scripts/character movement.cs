dc47a90a99beb00f56e1b8c82bf471c84f86f56f
svtozq <rpenea@esiee-it>
2025-01-13 16:14:35 +0100

    create script camera

---
+++ Assets/Characters/Scripts/character movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpForce = 5.0f;
+    public LayerMask groundLayer; 
+    public Transform groundCheck;
+    private Rigidbody rb;
+    private bool isGrounded;
+    public float groundCheckRadius = 0.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        
+        Vector3 newPosition = rb.position + transform.TransformDirection(movement) * Time.deltaTime;
+        rb.MovePosition(newPosition);
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
