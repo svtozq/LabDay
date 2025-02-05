76b2e52944189e1c7783ca3f7f0bea0cd9876949
svtozq <rpenea@esiee-it>
2025-02-05 15:25:15 +0100

    gun fire done

--- Assets/Scripts/GunSystem.cs
+++ Assets/Scripts/GunSystem.cs
@@ -14,6 +14,7 @@ public class GunSystem : MonoBehaviour
     bool shooting, readyToShoot, reloading;
 
     public Camera fpsCam;
+	public Transform cameraTransform;
     public Transform attackPoint;
     public RaycastHit rayHit;
     public LayerMask whatIsEnnemy;
@@ -35,6 +36,8 @@ public class GunSystem : MonoBehaviour
         myInput();
         
         text.SetText(bulletsLeft + " / " + magazineSize);
+		transform.rotation = Quaternion.Euler(cameraTransform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+		attackPoint.rotation = transform.rotation;
     }
     
     private void myInput()
@@ -58,17 +61,21 @@ public class GunSystem : MonoBehaviour
         float y = Random.Range(-spread, spread);
 
         Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+		Debug.DrawRay(attackPoint.position, direction * range, Color.red, 1f);
         
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range, whatIsEnnemy))
         {
-            Debug.Log(rayHit.collider.name);
+            Debug.Log("Hit : " + rayHit.collider.name);
 
             if (rayHit.collider.CompareTag("Ennemy"))
                 rayHit.collider.GetComponent<ShootingAi>().TakeDamage(damage);
+
+			Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
         }
         // camShake.Shake(camShakeMagnitude, camShakeDuration);
 
-        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
+       
         Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
         bulletsLeft--;
         bulletsShot--;
