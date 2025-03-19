f609cd5aabd34668c9b3cb48b51b9f8d26ef1f53
Arielpods <ariela.agba@edu.esiee-it.fr>
2025-03-19 12:49:37 +0100

    adding settings

---
+++ Assets/Scripts/Settings/Canvasmenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canvasmenu : MonoBehaviour
+{
+    public GameObject canva;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canva.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
